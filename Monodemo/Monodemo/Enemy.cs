35980a2408fc48784f56996e932ded201a68b881
khadeidredean <khadeidredean@gmail.com>
2017-08-31 07:49:28 -0600

    Enemy Collision

--- Monodemo/Monodemo/Enemy.cs
+++ Monodemo/Monodemo/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,9 @@ namespace Monodemo
 
             float enemyMoveSpeed;
 
+        public Rectangle enemyRec;
+        public Vector2 velocity;
+
             public void Initialize(Texture2D texture, Vector2 position)
 
             {
@@ -54,6 +58,48 @@ namespace Monodemo
                 Value = 100;
 
             }
+
+            public void DetectCol(Block block)
+        {
+            if (enemyRec.Intersects(block.blockRec))
+            {
+                DetectPixelCol(block);
+                Debug.WriteLine("Enemy Rec Collision");
+
+            }
+        }
+
+        private void DetectPixelCol(Block block)
+        {
+            Color[] sourceColors = new Color[EnemyTexture.Width * EnemyTexture.Height];
+            EnemyTexture.GetData(sourceColors);
+            Color[] targetColors = new Color[block.blockTexture.Width * block.blockTexture.Height];
+            block.blockTexture.GetData(targetColors);
+
+            int left = Math.Max(enemyRec.Left, block.blockRec.Left);
+            int top = Math.Max(enemyRec.Top, block.blockRec.Top);
+            int width = Math.Min(enemyRec.Right, block.blockRec.Right) - left;
+            int height = Math.Min(enemyRec.Bottom, block.blockRec.Bottom) - top;
+
+            Rectangle intersectingRec = new Rectangle(left, top, width, height);
+
+            for (int x = intersectingRec.Left; x < intersectingRec.Right; x++)
+            {
+                for (int y = intersectingRec.Top; y < intersectingRec.Bottom; y++)
+                {
+                    Color sourceColor = sourceColors[(x - enemyRec.Left) + (y - enemyRec.Top) * Width];
+                    Color targetColor = targetColors[(x - block.blockRec.Left) + (y - block.blockRec.Top) * block.Width];
+                    if (sourceColor.A > 0 && targetColor.A > 0)
+                    {
+                        Debug.WriteLine("Enemy Pixel Collision");
+                        Position.X -= velocity.X;
+                        Position.Y += velocity.Y;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
 
 
             public void Update(GameTime gametime)
